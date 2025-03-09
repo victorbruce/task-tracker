2375244aa89b0bcc7200336f29c3fb7304f57ce4
Victor Bruce <victorbruce82@gmail.com>
2025-03-09 13:14:04 +0000

    feat: add task form component and style

---
+++ src/components/TaskForm/index.tsx
@@ -0,0 +1,37 @@
+import { FormEvent, useRef } from "react";
+import style from "./TaskForm.module.css";
+
+const TaskForm = () => {
+  const taskInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (taskInputRef.current) {
+      console.log("task:", taskInputRef.current.value);
+      taskInputRef.current.value = "";
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={style.form}>
+      <label htmlFor="taskInput" className={style.srOnly}>
+        Task:
+      </label>
+      <input
+        ref={taskInputRef}
+        id="taskInput"
+        type="text"
+        name="task"
+        className={style.textInput}
+        placeholder="Add a task"
+        required
+      />
+      <button className={style.btn} type="submit" aria-label="Add Task">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default TaskForm;
