fc5bbade32b15511b596f8a52a7ded503b3a4bce
Victor Bruce <victorbruce82@gmail.com>
2025-03-09 17:17:34 +0000

    feat: add task to local storage after form submit

--- src/components/TaskForm/index.tsx
+++ src/components/TaskForm/index.tsx
@@ -1,14 +1,21 @@
 import { FormEvent, useRef } from "react";
+import { useTaskContext } from "../../context/TaskContext";
 import style from "./TaskForm.module.css";
 
 const TaskForm = () => {
   const taskInputRef = useRef<HTMLInputElement>(null);
+  const { addTask } = useTaskContext();
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
     if (taskInputRef.current) {
       console.log("task:", taskInputRef.current.value);
+      addTask({
+        id: Date.now(),
+        title: taskInputRef.current.value,
+        priority: "Medium",
+      });
       taskInputRef.current.value = "";
     }
   };
