df95be6bd87ea8097166b5d1b16898e69d6e25fd
Victor Bruce <victorbruce82@gmail.com>
2025-03-10 03:05:49 +0000

    feat(SearchTask): add search functionality to filter tasks

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,12 +1,20 @@
-import Task from "../Task";
+import { useState } from "react";
 import { useTaskContext } from "../../context/TaskContext";
 import style from "./Tasks.module.css";
 
+// components
+import Task from "../Task";
+import SearchTask from "../SearchTask";
+
 const Tasks = () => {
   const { tasks, priorityFilter } = useTaskContext();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const filteredTasks = tasks.filter(
-    (task) => priorityFilter === "All" || task.priority === priorityFilter
+    (task) =>
+      ((priorityFilter === "All" || task.priority === priorityFilter) &&
+        task.title.toLowerCase().includes(searchQuery.toLowerCase())) ||
+      task.description?.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   if (!filteredTasks.length) {
@@ -14,11 +22,14 @@ const Tasks = () => {
   }
 
   return (
-    <ul aria-label="tasks" className={style.tasks}>
-      {filteredTasks.map((task) => (
-        <Task key={task.id} task={task} />
-      ))}
-    </ul>
+    <div>
+      <SearchTask onSearch={setSearchQuery} />
+      <ul aria-label="tasks" className={style.tasks}>
+        {filteredTasks.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
