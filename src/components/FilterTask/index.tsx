16fd2c755bfe4bb7f7112d4e802ced9ed0cc4cdf
Victor Bruce <victorbruce82@gmail.com>
2025-03-10 12:36:42 +0000

    style: improve the look of the filter, search and task container components

--- src/components/FilterTask/index.tsx
+++ src/components/FilterTask/index.tsx
@@ -1,18 +1,23 @@
 import { useTaskContext } from "../../context/TaskContext";
+import style from "./FilterTask.module.css";
 
 const FilterTask = () => {
   const { priorityFilter, setPriorityFilter } = useTaskContext();
 
   return (
-    <select
-      value={priorityFilter}
-      onChange={(e) => setPriorityFilter(e.target.value)}
-    >
-      <option value="All">All</option>
-      <option value="High">High</option>
-      <option value="Medium">Medium</option>
-      <option value="Low">Low</option>
-    </select>
+    <div className={style.selectContainer}>
+      <select
+        value={priorityFilter}
+        onChange={(e) => setPriorityFilter(e.target.value)}
+        className={style.select}
+      >
+        <option value="All">All</option>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <div className={style.arrow}></div>
+    </div>
   );
 };
 
